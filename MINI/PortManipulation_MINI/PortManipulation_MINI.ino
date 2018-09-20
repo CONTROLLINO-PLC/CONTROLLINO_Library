8973f14e6c2499b2fd1b7587712b6d20b6602b70
CONTROLLINO <24877193+CONTROLLINO-Support@users.noreply.github.com>
2018-09-20 09:33:50 +0200

    Correction of comments
    As CONTROLLINO has all pins with Arduino numbers, the comment about "non-arduino pins" has been removed.

--- MINI/PortManipulation_MINI/PortManipulation_MINI.ino
+++ MINI/PortManipulation_MINI/PortManipulation_MINI.ino
@@ -1,28 +1,16 @@
 #include <Controllino.h>  /* Usage of CONTROLLINO library allows you to use CONTROLLINO_xx aliases in your sketch. */
 
 /*
-  CONTROLLINO - Port Manipulation for MINI, Version 01.00
+  CONTROLLINO - Port Manipulation for MINI, Version 01.01
   The sketch is relevant only for CONTROLLINO variant MINI!
 
   Turns on and off four digital outputs of CONTROLLINO, which also blinks with CONTROLLINO´s status LEDs for the outputs.
   It explains how to control the microcontroller´s pins directly via port registers.
+  This is an alternative to Arduino functions pinMode() and digitalWrite().
   
   This know how may be useful if you need to control the lines in more efficient way.
 
   Please, see https://www.arduino.cc/en/Reference/PortManipulation for some theoretical background.
-
-  List of non-Arduino pins used in CONTROLLINO devices:
-  ----------------------------------------------------
-   MINI - none (= all used pins have their Arduino and CONTROLLINO "nickname")
-   MAXI - RS485 serial bus /RE (output)
-        - RS485 serial bus  DE (output)
-        - Ethernet /SS (output) - Ethernet library handles this pin for you
-        - Ethernet /INT (input)
-        - Overload /OVL (input)
-        - Real Time Clock RTC /SS (output)
-        - Real Time Clock RTC /INT (input)
-   MEGA - the same like for MAXI plus
-        - Digital Outputs 20,21,22 and 23 (output)
 
   IMPORTANT INFORMATION!
   Please, select proper target board in Tools->Board->Controllino MINI before Upload to your CONTROLLINO.
@@ -30,7 +18,7 @@
 
   This example sketch does not require any additional parts, equipment, wires or power supply. Just your CONTROLLINO, USB cable (type A-B) and a PC.
  
-  Created 13 Dec 2016
+  Created 13 Dec 2016, Updated 20.09.2018
   by Lukas
 
   https://controllino.biz/
@@ -84,5 +72,5 @@ void loop() {
 
 /* End of the example. Visit us at https://controllino.biz/ or  https://github.com/CONTROLLINO-PLC/CONTROLLINO_Library or contact us at info@controllino.biz if you have any questions or troubles. */
 
-/* 2016-12-14: The sketch was successfully tested with Arduino 1.6.13, Controllino Library 1.0.0 and CONTROLLINO MINI. */
+/* 2018-09-20: The sketch was successfully tested with Arduino 1.8.5, Controllino Library 3.0.2 and CONTROLLINO MINI. */
 
