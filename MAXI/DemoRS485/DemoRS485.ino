6563cde323e7685e174e4e6457f036f58f6a5c11
CONTROLLINO <24877193+CONTROLLINO-Support@users.noreply.github.com>
2018-09-20 10:29:27 +0200

    Reworked example beacuse of updated API
    Using more comfortable way of RS485 handling.

--- MAXI/DemoRS485/DemoRS485.ino
+++ MAXI/DemoRS485/DemoRS485.ino
@@ -2,7 +2,7 @@
 #include <Controllino.h>  /* Usage of CONTROLLINO library allows you to use CONTROLLINO_xx aliases in your sketch. */
 
 /*
-  CONTROLLINO - Demonstration of RS485 interface usage, Version 01.00
+  CONTROLLINO - Demonstration of RS485 interface usage, Version 02.00
 
   Compatible with CONTROLLINO MAXI and MEGA.
 
@@ -11,9 +11,9 @@
   
   (Refer to https://github.com/CONTROLLINO-PLC/CONTROLLINO_Library if you do not see the CONTROLLINOs in the Arduino IDE menu Tools->Board.)
 
-  This example sketch requires external 12V or 24V power supply for your CONTROLLINO and RS485 communication partner device (e.g. another CONTROLLINO :-) ). 
+  This example sketch requires external 12V or 24V power supply for your CONTROLLINO and RS485 communication partner device (e.g. RS485-USB converter and PC). 
 
-  Created 12 Jan 2017
+  Created 12 Jan 2017, Updated 20.09.2018
   by Lukas
 
   https://controllino.biz/
@@ -21,90 +21,38 @@
   (Check https://github.com/CONTROLLINO-PLC/CONTROLLINO_Library for the latest CONTROLLINO related software stuff.)
 */
 
-/* This function will test RS485 functionality. Possible modes are 0-2. 0 is all enabled, sended message should come back, 1 means RE is low,  so message should not be recieved, 
-2 means DE is disabled, so no message should be sent at all.*/
-void TestRS485 (int mode)
-{
-  DDRJ = DDRJ | B01100000;
-  PORTJ = PORTJ & B10011111;
-  /*pinMode(CONTROLLINO_RS485_TX, OUTPUT);
-  pinMode(CONTROLLINO_RS485_RX, INPUT);
-  pinMode(CONTROLLINO_RS485_RE, OUTPUT);
-  pinMode(CONTROLLINO_RS485_DE, OUTPUT);*/
-  switch (mode)
-  {
-    case 0:
-      PORTJ = PORTJ & B10011111;
-      PORTJ = PORTJ | B01000000;
-      /*digitalWrite (CONTROLLINO_RS485_RE, HIGH);
-      digitalWrite (CONTROLLINO_RS485_DE, HIGH);*/
-      delay (10);
-      Serial.println ("Sending test message, expected to return;"); 
-      Serial3.print("UUUUU Controllino RS485 test Message.UUUUU");
-      break;
-      
-    case 1:
-      PORTJ = PORTJ & B10011111;
-      PORTJ = PORTJ | B01100000;
-      /*digitalWrite (CONTROLLINO_RS485_RE, LOW);
-      digitalWrite (CONTROLLINO_RS485_DE, HIGH);*/
-      delay (10);
-      Serial.println ("Sending test message, not expected to return;");
-      Serial3.print("UUUUU Controllino RS485 test Message.UUUUU");
-      break;
-      
-    case 2:
-      PORTJ = PORTJ & B10011111;
-      PORTJ = PORTJ | B00100000;
-     /* digitalWrite (CONTROLLINO_RS485_RE, HIGH);
-      digitalWrite (CONTROLLINO_RS485_DE, LOW);*/
-      delay (10);
-      Serial.println ("Sending test message, not expected to be sended;"); 
-      Serial3.print("UUUUU Controllino RS485 test Message.UUUUU");
-      break;
-      
-    default:
-      Serial.println("Wrong mode!");
-      return; 
-  }
-}
-
-/* This function enters loop and waits for any incoming data on serial 3.*/
-void RecieveRS485()
-{
-  Serial.println("Recieving RS485.");
-  while(true)
-  {
-    if (Serial3.available()) 
-    {
-      // print the new byte:
-      Serial.print((char)Serial3.read()); 
-    }
-  }
-}
+int incomingByte = 0;   // for incoming serial data
 
 void setup() {
-  /* Here we initialize USB serial at 9600 baudrate for reporting */
+  
+  /* Initialize serial port for debug messages. */
   Serial.begin(9600);
-  /* Here we initialize RS485 serial at 9600 baudrate for communication */
-  Serial3.begin(9600);
-  /* This will initialize Controllino RS485 pins */
-  Controllino_RS485Init();
+  
+  /* Initialize CONTROLLINO RS485 direction control DE/RE pins and Serial3 */
+  Controllino_RS485Init(9600);
+  Controllino_RS485RxEnable();
 
+  Serial.println("Recieving RS485...");
 }
 
 void loop() {
-  /* Use only one of the two functions, either send or recieve */
-  // RecieveRS485();
   
-  /* Send test. 2 seconds apart sending messages with 3 types of different settings. Check the function comment for more infromation */
-  TestRS485(0);
-  delay(2000);
-  TestRS485(1);
-  delay(2000);
-  TestRS485(2);
-  delay(2000);
+  // send data only when you receive data:
+  if (Serial3.available() > 0)
+  {
+    // read the incoming byte from RS485
+    incomingByte = Serial3.read();
+    // say what you got:
+    Serial.print("I received (DEC): ");
+    Serial.println(incomingByte, DEC);
 
+    // and send it back to RS485
+    Controllino_RS485TxEnable();
+    Serial3.write(incomingByte);
+    Serial3.flush(); // wait until the trasmission is complete
+    Controllino_RS485RxEnable();
+  }
+  
 }
 
-/* 2017-01-12: The sketch was successfully tested with Arduino 1.6.13, Controllino Library 1.0.0 and CONTROLLINO MAXI and MEGA. */
+/* 2018-09-20: The sketch was successfully tested with Arduino 1.8.5, Controllino Library 3.0.2 and CONTROLLINO MAXI and MEGA. */
