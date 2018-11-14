fe52a73a4e8338bc1d8b185fe82f42d7ccc8cba2
CONTROLLINO <24877193+CONTROLLINO-Support@users.noreply.github.com>
2018-11-14 23:06:25 +0100

    Added Controllino_SetTimeDateStrings
    Added Controllino_SetTimeDateStrings to allow comfortable __DATE__ and  __TIME__ macros usage.

--- Controllino.cpp
+++ Controllino.cpp
@@ -15,6 +15,7 @@
 
 
 #include "Controllino.h"
+#include <stdint.h>
 
 boolean isRTCInitialized = false;
 
@@ -380,6 +381,66 @@ char Controllino_PrintTimeAndDate( void )
 	{
 		return -1;//RTC chip was initialized properly, return -1
 	}
+}
+
+/* This function is taken from https://github.com/adafruit/RTClib */
+/* Released to the public domain! Enjoy! */
+static uint8_t conv2d(const char* p)
+{
+    uint8_t v = 0;
+    if ('0' <= *p && *p <= '9')
+        v = *p - '0';
+    return 10 * v + *++p - '0';
+}
+
+/* This function is taken from https://github.com/adafruit/RTClib */
+/* Released to the public domain! Enjoy! */
+/* number of days since 2000/01/01, valid for 2001..2099 */
+static uint16_t date2days(uint16_t y, uint8_t m, uint8_t d)
+{
+    if (y >= 2000)
+        y -= 2000;
+    uint16_t days = d;
+    for (uint8_t i = 1; i < m; ++i)
+        days += pgm_read_byte(daysInMonth + i - 1);
+    if (m > 2 && y % 4 == 0)
+        ++days;
+    return days + 365 * y + (y + 3) / 4 - 1;
+}
+
+/* This function is taken from https://github.com/adafruit/RTClib */
+/* Released to the public domain! Enjoy! */
+uint8_t dayOfTheWeek(uint8_t yOff,uint8_t  m, uint8_t d)
+{    
+    uint16_t day = date2days(yOff + 2000, m, d);
+    return (day + 6) % 7; // Jan 1, 2000 is a Saturday, i.e. returns 6
+}
+
+/* This function is inspired by https://github.com/adafruit/RTClib */
+/* Released to the public domain! Enjoy! */
+char Controllino_SetTimeDateStrings(const char* date, const char* time)
+{
+    // sample input: date = "Dec 26 2009", time = "12:34:56"
+    uint8_t yOff, m, d, hh, mm, ss;
+    
+    yOff = conv2d(date + 9);
+    // Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec 
+    switch (date[0]) {
+        case 'J': m = (date[1] == 'a') ? 1 : ((date[2] == 'n') ? 6 : 7); break;
+        case 'F': m = 2; break;
+        case 'A': m = date[2] == 'r' ? 4 : 8; break;
+        case 'M': m = date[2] == 'r' ? 3 : 5; break;
+        case 'S': m = 9; break;
+        case 'O': m = 10; break;
+        case 'N': m = 11; break;
+        case 'D': m = 12; break;
+    }
+    d = conv2d(date + 4);
+    hh = conv2d(time);
+    mm = conv2d(time + 3);
+    ss = conv2d(time + 6);
+
+    return Controllino_SetTimeDate(d,dayOfTheWeek(yOff, m, d),m,yOff,hh,mm,ss);
 }
 
 #if defined(CONTROLLINO_MAXI) || defined(CONTROLLINO_MEGA)
