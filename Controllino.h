3715dabd7fa374c3549964bf4be63a6d37f1de43
Charles Miller <charlie@charliemiller.me.uk>
2021-08-01 13:08:13 +0100

    Fix params

--- Controllino.h
+++ Controllino.h
@@ -1131,13 +1131,8 @@ char Controllino_SetRTCSS(char mode);
  *  See http://www.microcrystal.com/images/_Product-Documentation/02_Oscillator_&_RTC_Modules/02_Application_Manual/RV-2123-C2_App-Manual.pdf
  *  for more information.
  *
- *  @param aDay pointer to day 01 - 31
- *  @param aWeekDay pointer to weekday 00 - 06
- *  @param aMonth pointer to month 01 - 12
- *  @param aYear pointer to year 00 - 99
  *  @param aHour pointer to hours 01 - 12, or 00 - 23 (depending of 12H/24H mode)
  *  @param aMinute pointer to minutes 00 - 59
- *  @param aSecond pointer to seconds 00 - 59
  *  
  *  @return Returns 0 when succeeded, or -1 if the RTC library was not initialized before
  */
@@ -1146,7 +1141,7 @@ char Controllino_SetAlarm(unsigned char *aHour, unsigned char *aMinute);
 
 /** @brief Clears alarm interrupt on the RTC chip (RV-2123)
  *
- *  @return Returns seconds 0, or -1 if the RTC library was not initialized before
+ *  @return Returns 0 when succeeded, or -1 if the RTC library was not initialized before
  */
 char Controllino_ClearAlarm( void );
 
